354b456cf5777d13fec7a76f71bbe1d8ff314b84
AndreaCorona523 <a01366768@itesm.mx>
2022-05-06 18:48:04 -0500

    first commit

--- assets/js/inicio.js
+++ assets/js/inicio.js
@@ -1,5 +1,6 @@
+var modal = document.getElementsByClassName("modal__container");
+
 //Hiddes modal window when clicked in home page
-function closeModal(){
-    var modal = document.getElementsByClassName("modal__container");
+modal[0].onclick = function(){
     modal[0].style.display = "none";
 }
